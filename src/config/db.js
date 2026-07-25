517e39c96e99f6121791106e8331118a46146acb
IsiakaOladayo <oladayoisiaq@gmail.com>
2026-07-25 07:32:18 +0100

    Database password debugging

--- src/config/db.js
+++ src/config/db.js
@@ -14,3 +14,8 @@ pool.connect()
     .catch(err => console.error('Database connection error', err));
 
 module.exports = pool;
+
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
