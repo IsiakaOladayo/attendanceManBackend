def10fe89a6c5d374b8a3f6dc03265bdae53b0c3
IsiakaOladayo <oladayoisiaq@gmail.com>
2026-07-25 07:41:40 +0100

    Troubleshooting db js

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,23 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
+console.log({
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_USER: process.env.DB_USER,
+    DB_NAME: process.env.DB_NAME,
+    DATABASE_URL: process.env.DATABASE_URL ? "SET" : "NOT SET"
 });
 
-pool.connect()
-    .then(() => console.log("PostgreSQL connected successfully"))
-    .catch(err => console.error("Database connection error", err));
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 module.exports = pool;
